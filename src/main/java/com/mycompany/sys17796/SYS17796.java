61344528785cffa5149f59b12b41499cc1613da9
Rasheel <Rasheel@KINGKONGSLIDEONMYKNOB>
2023-06-16 22:48:59 -0400

    first commit

---
+++ src/main/java/com/mycompany/sys17796/SYS17796.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sys17796;
+
+/**
+ *
+ * @author Rasheel
+ */
+public class SYS17796 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
