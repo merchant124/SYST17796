8d34264785c100b3a60f521229122c21c7f2dc94
Rasheel <Rasheel@KINGKONGSLIDEONMYKNOB>
2023-06-17 19:06:45 -0400

    Second commit

---
+++ src/main/java/Studentlist/Studentlistt.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Studentlist;
+
+/**
+ *
+ * @author Rasheel
+ */
+public class Studentlistt {
+    public class CardGame {
+    public static void main (String args[]) {
+    System.out.println("hellll");
+}
+}
+}
